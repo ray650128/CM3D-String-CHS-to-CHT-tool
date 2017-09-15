4c074f8b70aaf3794ef68af85a34d151e4a848a4
Raymond Yang <ray650128@gmail.com>
2017-09-15 17:45:00 +0800

    1st commit

--- StringConvertor/Form1.Designer.cs
+++ StringConvertor/Form1.Designer.cs
@@ -28,12 +28,186 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtOrig = new System.Windows.Forms.TextBox();
+            this.txtTrans = new System.Windows.Forms.TextBox();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.btnTrans = new System.Windows.Forms.Button();
+            this.btnAllConv = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 135);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(89, 292);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Original";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(13, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Translation";
+            // 
+            // txtOrig
+            // 
+            this.txtOrig.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtOrig.Location = new System.Drawing.Point(12, 32);
+            this.txtOrig.Name = "txtOrig";
+            this.txtOrig.Size = new System.Drawing.Size(600, 23);
+            this.txtOrig.TabIndex = 3;
+            // 
+            // txtTrans
+            // 
+            this.txtTrans.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtTrans.Location = new System.Drawing.Point(12, 77);
+            this.txtTrans.Name = "txtTrans";
+            this.txtTrans.Size = new System.Drawing.Size(600, 23);
+            this.txtTrans.TabIndex = 4;
+            // 
+            // btnUp
+            // 
+            this.btnUp.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnUp.Location = new System.Drawing.Point(107, 106);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(75, 23);
+            this.btnUp.TabIndex = 5;
+            this.btnUp.Text = "上一筆";
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnDown.Location = new System.Drawing.Point(188, 106);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(75, 23);
+            this.btnDown.TabIndex = 6;
+            this.btnDown.Text = "下一筆";
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 16;
+            this.listBox2.Location = new System.Drawing.Point(107, 135);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(505, 292);
+            this.listBox2.TabIndex = 7;
+            this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.Location = new System.Drawing.Point(12, 106);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "刷新檔案列表";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button5.Location = new System.Drawing.Point(537, 106);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "儲存";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // btnTrans
+            // 
+            this.btnTrans.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnTrans.Location = new System.Drawing.Point(433, 106);
+            this.btnTrans.Name = "btnTrans";
+            this.btnTrans.Size = new System.Drawing.Size(98, 23);
+            this.btnTrans.TabIndex = 11;
+            this.btnTrans.Text = "簡繁轉換(單筆)";
+            this.btnTrans.UseVisualStyleBackColor = true;
+            this.btnTrans.Click += new System.EventHandler(this.btnTrans_Click);
+            // 
+            // btnAllConv
+            // 
+            this.btnAllConv.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnAllConv.Location = new System.Drawing.Point(329, 106);
+            this.btnAllConv.Name = "btnAllConv";
+            this.btnAllConv.Size = new System.Drawing.Size(98, 23);
+            this.btnAllConv.TabIndex = 12;
+            this.btnAllConv.Text = "簡繁轉換(全部)";
+            this.btnAllConv.UseVisualStyleBackColor = true;
+            this.btnAllConv.Click += new System.EventHandler(this.btnAllConv_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.btnAllConv);
+            this.Controls.Add(this.btnTrans);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnUp);
+            this.Controls.Add(this.txtTrans);
+            this.Controls.Add(this.txtOrig);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtOrig;
+        private System.Windows.Forms.TextBox txtTrans;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button btnTrans;
+        private System.Windows.Forms.Button btnAllConv;
     }
 }
 
--- StringConvertor/Form1.cs
+++ StringConvertor/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +14,147 @@ namespace StringConvertor
 {
     public partial class Form1 : Form
     {
+        string mScanDir = Application.StartupPath + "\\String";
+        string mScanType = "*.txt";
+        string mFullPath;
+        string[] txtFiles;
+        string txtConv;
+        string[] split;
+
+        List<string> list = new List<string>();
+
+        private const int LocaleSystemDefault = 0x0800;
+        private const int LcmapSimplifiedChinese = 0x02000000;
+        private const int LcmapTraditionalChinese = 0x04000000;
+
+        [DllImport("kernel32", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int LCMapString(int locale, int dwMapFlags, string lpSrcStr, int cchSrc,
+                                      [Out] string lpDestStr, int cchDest);
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ReloadFileList();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBox2.DataSource = null;
+            list.Clear();
+
+            mFullPath = mScanDir + "\\" + txtFiles[listBox1.SelectedIndex];
+
+            StreamReader sr = new StreamReader(mFullPath, System.Text.Encoding.UTF8);
+            while (sr.Peek() > -1)
+            {
+                //listBox2.Items.Add(sr.ReadLine());
+                list.Add(sr.ReadLine());
+            }
+            sr.Close();
+
+            listBox2.DataSource = list;
+            listBox2.SelectedIndex = 0;
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedIndex > 0)
+            {
+                String tempText = listBox2.SelectedItem.ToString();
+                split = tempText.Split(new Char[] { '\t' });
+                txtOrig.Text = split[0];
+                txtTrans.Text = split[1];
+            }
+        }
+
+        private void btnTrans_Click(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedIndex > 0)
+            {
+                txtConv = ToTraditional(txtTrans.Text);
+                txtTrans.Text = txtConv;
+                list[listBox2.SelectedIndex] = split[0] + '\t' + txtConv;
+                listBox2.DataSource = null;
+                listBox2.DataSource = list;
+
+                if (listBox2.SelectedIndex >= 0 && listBox2.SelectedIndex < listBox2.Items.Count - 1)
+                {
+                    listBox2.SelectedIndex++;
+                }
+            }
+        }
+
+        private void btnAllConv_Click(object sender, EventArgs e)
+        {
+            string[] temp;
+            for(int i = 1; i < list.Count; i++)
+            {
+                temp = list[i].Split(new Char[] { '\t' });
+                temp[1] = ToTraditional(temp[1]);
+                list[i] = temp[0] + '\t' + temp[1];
+            }
+            listBox2.DataSource = null;
+            listBox2.DataSource = list;
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedIndex <= listBox2.Items.Count && listBox2.SelectedIndex > 0)
+            {
+                listBox2.SelectedIndex--;
+            }
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            if (listBox2.SelectedIndex >= 0 && listBox2.SelectedIndex < listBox2.Items.Count - 1)
+            {
+                listBox2.SelectedIndex++;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ReloadFileList();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream(mFullPath, FileMode.Create);
+
+            StreamWriter sw = new StreamWriter(fs, System.Text.Encoding.UTF8);
+
+            //開始寫入
+            foreach (string line in list)
+                sw.WriteLine(line);
+
+            //清空緩衝區
+            sw.Flush();
+            //關閉流
+            sw.Close();
+            fs.Close();
+        }
+
+        public static string ToTraditional(string argSource)
+        {
+            var t = new String(' ', argSource.Length);
+            LCMapString(LocaleSystemDefault, LcmapTraditionalChinese, argSource, argSource.Length, t, argSource.Length);
+            return t;
+        }
+
+        private void ReloadFileList()
+        {
+            txtFiles = System.IO.Directory.GetFiles(mScanDir, @mScanType, System.IO.SearchOption.TopDirectoryOnly); // System.IO.SearchOption.AllDirectories
+            for (int i = 0; i < txtFiles.Length; i++)
+            {
+                txtFiles[i] = txtFiles[i].Replace(mScanDir + "\\", "");
+            }
+            this.listBox1.Items.Clear();
+            this.listBox1.Items.AddRange(txtFiles);
         }
     }
 }
